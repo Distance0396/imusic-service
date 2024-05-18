4b028521e6d0f6d241f3c7bc06030677530d1f23
Distance0396 <2352537896@qq.com>
2024-05-18 14:47:37 +0800

    初始化

---
+++ imusic-server/src/main/java/com/distance0/imusic/Application.java
@@ -0,0 +1,16 @@
+package com.distance0.imusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: XiangJing
+ * @date: 2024/5/14 上午1:05
+ * @description:
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
